463ec3d45329fff470df6c4348f53ab1722500ea
Samir <65616743+Chapali9a@users.noreply.github.com>
2021-04-13 14:21:42 +0000

    Revert "Seperating search engine logic from view page."
    This reverts commit 1919f370fa6e0334d64cd3d45bff1058772f2a50.

--- BlazorApp2/Pages/SearchEngineBase.cs
+++ BlazorApp2/Pages/SearchEngineBase.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -12,8 +11,5 @@ namespace BlazorApp2.Pages
 {
     public class SearchEngineBase
     {
-        [Required]
-        [MinLength(3, ErrorMessage = "Minimum of 3 characters required for the search!")]
-        public string SearchName { get; set; }
     }
 }
