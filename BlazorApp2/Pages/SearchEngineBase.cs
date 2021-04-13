1919f370fa6e0334d64cd3d45bff1058772f2a50
Samir <65616743+Chapali9a@users.noreply.github.com>
2021-04-13 14:05:23 +0000

    Seperating search engine logic from view page.

--- BlazorApp2/Pages/SearchEngineBase.cs
+++ BlazorApp2/Pages/SearchEngineBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace BlazorApp2.Pages
 {
     public class SearchEngineBase
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Minimum of 3 characters required for the search!")]
+        public string SearchName { get; set; }
     }
 }
