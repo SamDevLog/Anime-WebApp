4f929c19ab82b9b45e3d9a468dbcb0442529f7f1
Samir <Admin@SAMIR>
2021-11-15 16:55:35 +0100

    GlideJs implemented and CSS changes for responsive

--- BlazorApp2/wwwroot/js/glider.js
+++ BlazorApp2/wwwroot/js/glider.js
@@ -2,8 +2,20 @@ window.initializeGlider = (className) => {
     new Glide(className, {
         autoplay: 3000,
         bound: true,
+        rewind: true,
         gap: 40,
         hoverpause: true,
         perView: 2,
+        breakpoints: {
+            1200:{
+                perView: 3
+            },
+            1024: {
+              perView: 2
+            },
+            600: {
+              perView: 1
+            }
+          }
     }).mount()
 }
